5cd0d833769f656236ce9f7a89bd4865e4803c71
pokedigger8 <pokedigger8@gmail.com>
2022-08-26 00:01:12 +0100

    Added De-rank Command, Fixed Rankup Command (maybe

--- src/commands/tools/derank-enlisted.js
+++ src/commands/tools/derank-enlisted.js
@@ -2,9 +2,9 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName(`rankup-enlisted`)
-        .setDescription(`Rankup enlisted personnel`)
-        .addUserOption((user) => user.setName(`user`).setDescription(`Select the user who will be ranked up`).setRequired(true)),
+        .setName(`derank-enlisted`)
+        .setDescription(`derank enlisted personnel`)
+        .addUserOption((user) => user.setName(`user`).setDescription(`Select the user who will be ranked down`).setRequired(true)),
     async execute(interaction, client) {
 
         const enlistedRanks = [`OR-1`,`OR-2`,`OR-3`,`OR-4`,`OR-5`,`OR-6`,`OR-7`,`OR-8`,`OR-9`];
@@ -33,26 +33,25 @@ module.exports = {
                 }
             }
 
-            if(userRank.name == enlistedRanks[8]) {
+            if(userRank.name == enlistedRanks[0]) {
 
                 await interaction.reply({
-                    content: "ERROR: User is at max Enlisted Rank!", ephemeral: true,
+                    content: "ERROR: User is at lowest Enlisted Rank!", ephemeral: true,
                 })
 
             } else {
-                const nextRankPosition = userRankPosition + 1;
+                const prevRankPosition = userRankPosition - 1;
 
-                const nextRank = interaction.guild.roles.cache.find(role => role.name === enlistedRanks[nextRankPosition]);
+                const prevRank = interaction.guild.roles.cache.find(role => role.name === enlistedRanks[prevRankPosition]);
 
-                console.log(userRank);
                 myTarget.roles.remove(userRank.id);
 
-                myTarget.roles.add(nextRank.id);
+                myTarget.roles.add(prevRank.id);
 
-                workOrder.send(`Target(s): ${s1Role} \n Name: Sága A.I. \n Request Type(s): Enlisted Rankup \n Request Text: ${targetMember} has been promoted from ${userRank} to ${nextRank} \n Notes: Please Check the ORBAT Rank Structures for your next rank acronym ${targetMember}! If you are Standard OR-4. If you are Staff or leadership, Please select Corporal, if not, select Specialist rank!`);
+                workOrder.send(`Target(s): ${s1Role} \n Name: Sága A.I. \n Request Type(s): Enlisted Rankup \n Request Text: ${targetMember} has been demoted from ${userRank} to ${prevRank} \n Notes: Please Check the ORBAT Rank Structures for your rank acronym ${targetMember}! If you are Standard OR-4. If you are Staff or leadership, Please select Corporal, if not, select Specialist rank!`);
 
                 await interaction.reply({
-                    content: "SUCCESSFUL: User has been Ranked up!", ephemeral: true,
+                    content: "SUCCESSFUL: User has been Deranked!", ephemeral: true,
                 })
             }
         }
